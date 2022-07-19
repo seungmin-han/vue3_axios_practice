72fa4c63a91820ff3f886eb342f2d44709e75031
SEUNGMIN HAN <seungmin_han@kakao.com>
2022-07-19 17:01:49 +0900

    request interceptor 로직 추가

--- src/utils/Send.js
+++ src/utils/Send.js
@@ -1,21 +1,35 @@
+import { useListStore } from "@/stores/list";
 import axios from "axios";
 
-const instance = axios.create(
-    {
-        baseURL: 'http://localhost:4010/',
-    }
+const instance = axios.create({
+    baseURL: "http://localhost:4010/",
+});
+
+instance.interceptors.request.use(
+    function (request) {
+        if (request.url != "/login") {
+            const store = useListStore();
+            if (store.token == "")
+                throw new axios.Cancel("토큰이 존재하지 않습니다.");
+            else request.headers.token = store.token;
+        }
+
+        return request;
+    },
+
+    function (error) {
+        return Promise.reject(error);
+    },
 );
 
 instance.interceptors.response.use(
-    function (response)
-    {
+    function (response) {
         return response;
-    }
+    },
 
-    , function (error)
-    {
+    function (error) {
         return Promise.reject(error);
-    }
-)
+    },
+);
 
 export default instance;
