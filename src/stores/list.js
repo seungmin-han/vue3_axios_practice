f35b50a137d41dcfaf9e1055970b086039e2298c
SEUNGMIN HAN <seungmin_han@kakao.com>
2022-07-19 17:02:30 +0900

    token parameter 제거, 에러처리 추가

--- src/stores/list.js
+++ src/stores/list.js
@@ -1,142 +1,129 @@
 import Service from "@/api/Service";
+import axios from "axios";
 import { defineStore } from "pinia";
 import { reactive, ref } from "vue";
 
-
-export const useListStore = defineStore("saaa", () =>
-{
+export const useListStore = defineStore("saaa", () => {
     const token = ref("");
     const id = ref(null);
     const name = ref("");
-    const state = reactive(
-        {
-            list : reactive([]),
-        }
-    );
+    const state = reactive({
+        list: reactive([]),
+    });
 
-    function nameCheck() 
-    {
-        if(name.value==null || name.value=="") 
-        {
+    function nameCheck() {
+        if (name.value == null || name.value == "") {
             alert("name을 입력하세요.");
             return false;
         }
         return true;
     }
 
-    function idCheck() 
-    {
-        if(id.value==null || id.value=="") 
-        {
+    function idCheck() {
+        if (id.value == null || id.value == "") {
             alert("id를 입력하세요.");
-            return false;    
+            return false;
         }
         return true;
     }
 
-    function getToken()
-    {
-        // if (token.value != "")
-        // {
-        //     alert("토큰은 1회만 발급 가능합니다.");
-        //     return;  
-        // } 
+    function getToken() {
         Service.getToken()
-        .then((res) =>
-        {
-            token.value = res.data.response.token;
-            read();
-        })
-        .catch((error) =>
-        {
-            console.log('error', error.response.data.errorMessage);
-            warning(error.response);
-        })
+            .then((res) => {
+                token.value = res.data.response.token;
+                read();
+            })
+            .catch((error) => {
+                console.log(error);
+                warning(error.response);
+            });
     }
 
-    function create()
-    {   
+    function create() {
         if (!nameCheck()) return;
-        Service.create(token.value, name.value)
-        .then((res) =>
-        {
-            console.log(res);
-            read();
-        })
-        .catch((error) =>
-        {
-            console.log('error', error);
-            warning(error.response);
-        })
+        Service.create(name.value)
+            .then((res) => {
+                console.log(res);
+                read();
+            })
+            .catch((error) => {
+                console.log("error", error);
+                if (axios.isCancel(error)) {
+                    alert(error.message);
+                } else {
+                    warning(error.response);
+                }
+            });
     }
 
-    function update()
-    {
+    function update() {
         if (!nameCheck() || !idCheck()) return;
-        Service.update(token.value, id.value, name.value)
-        .then((res) =>
-        {
-            console.log(res);
-            read();
-        })
-        .catch((error) =>
-        {
-            console.log('error', error);
-            warning(error.response);
-        })
+        Service.update(id.value, name.value)
+            .then((res) => {
+                console.log(res);
+                read();
+            })
+            .catch((error) => {
+                console.log("error", error);
+                if (axios.isCancel(error)) {
+                    alert(error.message);
+                } else {
+                    warning(error.response);
+                }
+            });
     }
 
     function delete_() {
         if (!idCheck()) return;
-        Service.delete(token.value, id.value)
-        .then((res) =>
-        {
-            console.log(res);
-            read();
-        })
-        .catch((error) =>
-        {
-            console.log('error', error);
-            warning(error.response);
-        })
+        Service.delete(id.value)
+            .then((res) => {
+                console.log(res);
+                read();
+            })
+            .catch((error) => {
+                console.log("error", error);
+                if (axios.isCancel(error)) {
+                    alert(error.message);
+                } else {
+                    warning(error.response);
+                }
+            });
     }
 
-    function read()
-    {
-        Service.read(token.value)
-        .then((res) =>
-        {
-            console.log(res);
-            console.log("res:", res.data.response);
-            console.log("res.status:", res.data.status);
-            state.list = res.data.response;
-            id.value = "";
-            name.value = "";
-            if(res.data.status == 1) warning(res);
-        })
-        .catch((error) =>
-        {
-            console.log('error', error);
-            warning(error.response);
-        })
-        
+    function read() {
+        Service.read()
+            .then((res) => {
+                console.log(res);
+                console.log("res:", res.data.response);
+                console.log("res.status:", res.data.status);
+                state.list = res.data.response;
+                id.value = "";
+                name.value = "";
+                if (res.data.status == 1) warning(res);
+            })
+            .catch((error) => {
+                if (axios.isCancel(error)) {
+                    alert(error.message);
+                } else {
+                    warning(error.response);
+                }
+            });
     }
 
-    function warning(target)
-    {
-        alert(target.data.errorMessage); 
+    function warning(target) {
+        alert(target.data.errorMessage);
     }
 
     return {
-        token
-        , id
-        , name
-        , state
-        , getToken
-        , create
-        , update
-        , delete_
-        , read
-        , warning
-    }
-})
+        token,
+        id,
+        name,
+        state,
+        getToken,
+        create,
+        update,
+        delete_,
+        read,
+        warning,
+    };
+});
