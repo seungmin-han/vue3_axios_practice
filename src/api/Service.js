1a3db46cd50b9bbc4b64d84ddb3c34a04c49cd81
SEUNGMIN HAN <seungmin_han@kakao.com>
2022-07-19 17:03:52 +0900

    token parameter 제거, url 수정

--- src/api/Service.js
+++ src/api/Service.js
@@ -1,41 +1,45 @@
 import Send from "@/utils/Send.js";
 
 export default {
-    name: "Service"
-    , getToken()
-    {
+    name: "Service",
+    getToken() {
         return Send({
-            url: '/login'
-            , method: 'get'
-        })
-    }
-    , read(token)
-    {
+            url: "/login",
+            method: "get",
+        });
+    },
+    read() {
         return Send({
-            url: `/${token}/read`
-            , method: 'get'
-        })
-    }
-    , create(token, name)
-    {
+            url: `/api/read`,
+            method: "get",
+        });
+    },
+    create(name) {
         return Send({
-            url: `/${token}/create`
-            , method: 'post'
-            , data: {
-                name: name
-            }
-        })
-    }
-    , update(token, id, name)
-    {
+            url: `/api/create`,
+            method: "post",
+            data: {
+                name: name,
+            },
+        });
+    },
+    update(id, name) {
         return Send({
-            url: `/${token}/update`
-            , method: 'post'
-            , data: {
-                id: id
-                , name: name
-            }
-        })
-    }
-    
-}
+            url: `/api/update`,
+            method: "post",
+            data: {
+                id: id,
+                name: name,
+            },
+        });
+    },
+    delete(id) {
+        return Send({
+            url: `/api/delete`,
+            method: "post",
+            data: {
+                id: id,
+            },
+        });
+    },
+};
